935eceed0d12e3d5704619f5a647f48e03bf18d3
Matt Kelley <matt@dangerouspenguins.com>
2014-05-13 14:50:20 -0500

    Added SetupFiles

---
+++ SetupFiles/node/runSeleniumChrome.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar "c:\selenium\selenium239.jar" -Dwebdriver.chrome.driver="C:\selenium\chromedriver.exe" -role node  -hub http://10.1.1.59:4444/grid/register
