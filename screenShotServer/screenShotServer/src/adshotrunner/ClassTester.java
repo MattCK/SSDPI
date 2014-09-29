c7c93836beb5594d3a52b8b5606076b8368afb08
Matt Kelley <matt@dangerouspenguins.com>
2014-09-29 14:54:56 -0500

    moved off of shared workspace to shared project

---
+++ screenShotServer/screenShotServer/src/adshotrunner/ClassTester.java
@@ -0,0 +1,19 @@
+package adshotrunner;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
+import adshotrunner.StoryFinder.Calculator;
+
+public class ClassTester {
+	
+	public static void main(String[] args) throws JSONException, MalformedURLException, URISyntaxException {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> some = new StoryFinder("http://www.boston.com/entertainment").Calculator().getScoredStories();
+	}
+	
+
+}
