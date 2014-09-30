644d33bc8ab379b77fe0700619075a5ce110c671
Matt Kelley <matt@dangerouspenguins.com>
2014-09-30 12:35:36 -0500

    working periodic menu grabber

--- frontend/menuGrabberAutoRun.php
+++ frontend/menuGrabberAutoRun.php
@@ -7,41 +7,51 @@ Class menuGrabberProxy Extends menuGrabber {
 
 	public function getRankedMenusFromURLList($urlList) {
 		
-		return parent::getRankedMenusFromManyURLs($urlList)
+		return parent::getRankedMenusFromManyURLs($urlList);
 
 	}
 	
+	public function InsertDomainsWithMenusFromList($domainMenuList){
+	
+		return parent::InsertDomainsWithMenus($domainMenuList);
+	
+	}
+	
+	public function retrieveDomainListFromDatabase() {
+		
+		//Retrieve the 
+		$domainGrabResult = databaseQuery("SELECT * FROM menuDomains");
+		$domainList = array();
+		while ($curRow = $domainGrabResult->fetch_assoc()) {
+		    $domainList[$curRow['MND_domain']] = $curRow['MND_lastUpdated'];
+		}
+		return $domainList;
+	}
 	
 }
 
 header("Content-Type: text/plain");
-
-
 $database = new MySQLDatabase('adshotrunner.c4gwips6xiw8.us-east-1.rds.amazonaws.com', 'adshotrunner', 'xbSAb2G92E', 'adshotrunner');
 
 $menuGrabber = new MenuGrabberProxy();
-$domains = $menuGrabber->getDomainMenus(['boston.com', 'nytimes.com', 'cnn.com']);
-//$domains = $menuGrabber->retrieveManyDomainMenusFromDatabase(['omahaworldherald.com', 'chicagotribune.com']);
 
+//$domainArray = array();
+$domainAndTimeArray = $menuGrabber->retrieveDomainListFromDatabase();
+//print_r($domainAndTimeArray);
+asort($domainAndTimeArray);
+//$domainAndTimeArray = array_reverse($domainAndTimeArray);
+//print_r($domainAndTimeArray);
+$domainAndTimeArrayLength = count($domainAndTimeArray);
+//since we'll be grabbing 1/3 of the list every 3 days we need to slice the array
+// the plus one ensures we always get at least 1/3 of the domain list when it doesn't divide evenly
+$thirdOfDomainLength = floor($domainAndTimeArrayLength/3) + 1;
 
+$targetDomains = array_slice($domainAndTimeArray,0,  $thirdOfDomainLength);
+//print_r($targetDomains);
 
-print_r($menuGrabber->getRankedMenusFromURLList([
-			'nytimes.com', 
-			'boston.com',
-			'omahaworldherald.com',
-			'nypost.com',
-			'latimes.com',
-			'cnn.com',
-			'theguardian.com',
-			'chicagotribune.com',
-			'nypost.com',
-			'bbc.com',
-			'newyorker.com',
-			'newyorker.com',
-			'abcnews.go.com',
-			'bismarktribune.com',
-			'cincinnati.com'
-		]));
+$domainArray = array_keys($targetDomains);
+$menuGrabber->InsertDomainsWithMenusFromList($menuGrabber->getRankedMenusFromURLList($domainArray));
+print("Done with the oldest third of the domain list");
 		
 exit();
 
