57de0d1bac66f9223e6403de4efec985a5a27c66
mattck <matt@dangerouspenguins.com>
2016-12-19 15:46:18 -0600

    shottester exception catching

--- screenShotServer/sssEclipseProject/src/adshotrunner/techpreview/ScreenshotTest.java
+++ screenShotServer/sssEclipseProject/src/adshotrunner/techpreview/ScreenshotTest.java
@@ -9,18 +9,46 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class ScreenshotTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		 System.setProperty("webdriver.chrome.driver", "/home/juicio/Desktop/selenium/chromedriver");
+		 //System.setProperty("webdriver.chrome.driver", "/home/matt/Work/DangerousPenguins/localTesting/chromedriver");
 
-		  WebDriver driver = new ChromeDriver();
-		  driver.manage().timeouts().pageLoadTimeout(7000, TimeUnit.MILLISECONDS);
-		  driver.get("http://www.wkyc.com/");
+		DesiredCapabilities driverCapabilities = DesiredCapabilities.chrome();
+		ChromeOptions driverOptions = new ChromeOptions();
+		
+		driverOptions.addArguments("hide-scrollbars");
+		driverOptions.addArguments("start-maximized");
+		
+		driverCapabilities.setCapability(ChromeOptions.CAPABILITY, driverOptions);
+		
+		 
+		 WebDriver driver = new ChromeDriver(driverCapabilities);
+		  driver.manage().timeouts().pageLoadTimeout(4000, TimeUnit.MILLISECONDS);
+		  System.out.println("about to get website");
+		  try{
+			  driver.get("http://www.wkyc.com/");
+		  }
+		  catch (Exception e){
+			  System.out.println("caught get site exception : " + e);
+		  }
+		  System.out.println("After Get before take screenshot");
+		  driver.manage().timeouts().pageLoadTimeout(40000, TimeUnit.MILLISECONDS);
 		  //driver.get("http://www.chicagotribune.com/lifestyles/");
-		  File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		  try{
+			  File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			  System.out.println("After takescreenhot before rename");
+			  screenshot.renameTo(new File("screenshot.png"));
+			  System.out.println("after file rename");
+		  }
+		  catch (Exception e){
+			  System.out.println("caught screenshot exception : " + e);
+		  }
+		 
 		  //Thread.sleep(5000);  // Let the user actually see something!
 		  //WebElement searchBox = driver.findElement(By.name("q"));
 		  //searchBox.sendKeys("ChromeDriver");
